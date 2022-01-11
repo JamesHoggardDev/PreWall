5dfba6d859ea897ce2759e192d219e77fc3bff19
James <Jameshoggard.dev@gmail.com>
2022-01-11 16:09:26 -0500

    up3

--- src/routes/expenses.jsx
+++ src/routes/expenses.jsx
@@ -1,6 +1,9 @@
 export default function Expenses() {
   return (
-    <main style={{ padding: "1rem 0" }}>
+    <main
+      style={{ padding: "1rem 0" }}
+      
+    >
       <h2 style={{ color: "darkred" }}>Final Expenses</h2>
     </main>
   );
